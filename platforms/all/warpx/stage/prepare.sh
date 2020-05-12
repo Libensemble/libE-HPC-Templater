67ee39ea0ca64ec6d8153fafc727b4423d0a2d4a
jlnav <jnavarro@anl.gov>
2020-05-12 09:21:58 -0500

    copy sim input dir over in prepare script

--- platforms/all/warpx/stage/prepare.sh
+++ platforms/all/warpx/stage/prepare.sh
@@ -4,6 +4,7 @@ for dir in test_*; do
         cp warpx_simf.py $dir
         cp read_sim_output.py $dir
         cp write_sim_input.py $dir
+        cp -r sim $dir
         cd $dir
         chmod +x submit_*
         cd ..
