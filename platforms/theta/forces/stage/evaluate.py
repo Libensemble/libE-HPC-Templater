eac9a4898783a1409670418d3816320c8b53abd5
jlnav <jnavarro@anl.gov>
2020-10-01 16:55:43 -0500

    double time limit (for queue time), check for files instead of env vars, move directory-change for balsam

--- platforms/theta/forces/stage/evaluate.py
+++ platforms/theta/forces/stage/evaluate.py
@@ -8,9 +8,13 @@ from time import sleep
 from forces_support import test_libe_stats, test_ensemble_dir, check_log_exception
 
 sleeptime = 0
-limit = 1500
+limit = 3000
 
 print('Waiting on test completion for up to {} minutes...'.format(limit/60), flush=True)
+
+# If using Balsam, change to job-specific dir. Hopefully only one.
+if os.environ.get('BALSAM_DB_PATH'):
+    os.chdir(glob.glob(os.environ['BALSAM_DB_PATH'] + '/data/libe_workflow/job_run_libe_forces_*')[0])
 
 # Wait for env vars or files set by conclusion of run_libe_forces
 while sleeptime < limit:
@@ -22,17 +26,11 @@ while sleeptime < limit:
         print(' done.', end=" ", flush=True)
         break
 
-# If using Balsam, change to job-specific dir. Hopefully only one.
-if os.environ.get('BALSAM_DB_PATH'):
-    os.chdir(glob.glob(os.environ['BALSAM_DB_PATH'] + '/data/libe_workflow/job_run_libe_forces_*')[0])
-
 # Evaluate output files based on type of error (if any)
-if 'LIBE_EVALUATE_ERROR' in os.environ:
-    error = os.environ['LIBE_EVALUATE_ERROR']
-    if error == 'fail_on_sim':
-        test_libe_stats('Exception occurred\n')
-    elif error == 'fail_on_submit':
-        test_libe_stats('Task Failed\n')
+if 'FAIL_ON_SIM' in os.listdir('.'):
+    test_libe_stats('Exception occurred\n')
+elif 'FAIL_ON_SUBMIT' in os.listdir('.'):
+    test_libe_stats('Task Failed\n')
 
 # Evaluate ensemble directory
 if 'LIBE_EVALUATE_ENSEMBLE' in os.listdir('.'):
