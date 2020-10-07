6f40000aff160431cebb1cc91cbcbe82dbee639b
jlnav <jnavarro@anl.gov>
2020-10-07 17:17:02 -0500

    add headings and intro description for platform-test preparation instructions, define dictionary in new file

---
+++ instructions.py
@@ -0,0 +1,25 @@
+instructions = {
+('summit', 'warpx'):
+
+,
+('summit', 'forces'):
+
+,
+('bebop', 'warpx'):
+
+,
+('bebop', 'forces'):
+
+,
+('cori', 'warpx'):
+
+,
+('cori', 'forces'):
+
+,
+('theta', 'warpx'):
+
+,
+('theta', 'forces'):
+
+,}
