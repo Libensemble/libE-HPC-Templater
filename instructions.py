e52a8987f530b63539830f136d3e3f98b6a392be
jlnav <jnavarro@anl.gov>
2020-10-08 10:46:20 -0500

    complete preliminary instructions, format other strings, add prompt to continue templating or exit following instructions

--- instructions.py
+++ instructions.py
@@ -1,25 +1,44 @@
+
+
 instructions = {
 ('summit', 'warpx'):
+"1) Make sure WarpX is built, and the path in ./platforms/all/warpx/stage/all_machine_specs.py matches.\n" +
+"2) Make sure your Conda environment matches ./platforms/summit/platform.json\n" +
+"3) If wanted, make test-specific adjustments to the .json files in ./platforms/summit/warpx\n"
 
 ,
 ('summit', 'forces'):
+"1) Make sure your Conda environment matches ./platforms/summit/platform.json\n" +
+"2) If wanted, make test-specific adjustments to the .json files in ./platforms/summit/forces\n"
 
 ,
 ('bebop', 'warpx'):
-
+"1) Make sure WarpX is built, and the path in ./platforms/all/warpx/stage/all_machine_specs.py matches.\n" +
+"2) Make sure your Conda environment matches ./platforms/bebop/platform.json\n" +
+"3) If wanted, make test-specific adjustments to the .json files in ./platforms/bebop/warpx\n"
 ,
 ('bebop', 'forces'):
+"1) Make sure your Conda environment matches ./platforms/bebop/platform.json\n" +
+"2) If wanted, make test-specific adjustments to the .json files in ./platforms/bebop/forces\n"
 
 ,
 ('cori', 'warpx'):
-
+"1) Make sure WarpX is built, and the path in ./platforms/all/warpx/stage/all_machine_specs.py matches.\n" +
+"2) Make sure your Conda environment matches ./platforms/cori/platform.json\n" +
+"3) If wanted, make test-specific adjustments to the .json files in ./platforms/cori/warpx\n"
 ,
 ('cori', 'forces'):
+"1) Make sure your Conda environment matches ./platforms/cori/platform.json\n" +
+"2) If wanted, make test-specific adjustments to the .json files in ./platforms/cori/forces\n"
 
 ,
 ('theta', 'warpx'):
-
+"1) Make sure WarpX is built, and the path in ./platforms/all/warpx/stage/all_machine_specs.py matches.\n" +
+"2) Make sure your Conda environment matches ./platforms/theta/platform.json\n" +
+"3) If wanted, make test-specific adjustments to the .json files in ./platforms/theta/warpx\n"
 ,
 ('theta', 'forces'):
+"1) Make sure your Conda environment matches ./platforms/theta/platform.json\n" +
+"2) If wanted, make test-specific adjustments to the .json files in ./platforms/theta/forces\n"
 
-,}
+}
