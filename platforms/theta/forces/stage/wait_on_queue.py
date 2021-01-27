b4985ff36ae8fc5b58fbb12d1c832681844a1a36
jlnav <jnavarro@anl.gov>
2021-01-27 11:11:33 -0600

    wait_on_queue now waits until enough available nodes

--- platforms/theta/forces/stage/wait_on_queue.py
+++ platforms/theta/forces/stage/wait_on_queue.py
@@ -7,14 +7,20 @@ limit = 3600
 queue = os.environ['TEST_QUEUE']
 user = "csc250stms07"
 
+def check_queue_output():
+    return subprocess.run('qstat {}'.format(queue).split(), capture_output=True, text=True).stdout.split()
+
 def user_in_queue(user):
-    return user in subprocess.run('qstat {}'.format(queue).split(), capture_output=True, text=True).stdout.split()
+    return user in check_queue_output()
+
+def count_used_nodes():
+    return sum([int(i) for i in check_queue_output() if len(i) == 1])
 
 if __name__ == '__main__':
 
     print('Waiting on {} queue availability for up to {} minutes...'.format(queue, limit/60), flush=True)
 
-    while user_in_queue(user):
+    while user_in_queue(user) or count_used_nodes() >= 11:
         sleep(60)
         sleeptime += 60
         assert sleeptime < limit, "No availability in {} queue within the time limit.".format(queue)
