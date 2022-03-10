f6402c24897cfe623e41368887f978158d36af52
shudson <shudson@anl.gov>
2022-03-10 17:14:04 -0600

    Update fbpic submission script

--- platforms/all/run_libe_fbpic.py
+++ platforms/all/run_libe_fbpic.py
@@ -1,8 +1,7 @@
 import os
 import numpy as np
 from varying_parameters import varying_parameters
-from analysis_script import analyze_simulation, analyzed_quantities
-from mf_parameters import mf_parameters
+from analysis_script import analyze_simulation
 
 from libensemble.libE import libE
 from libensemble.tools import parse_args
@@ -10,21 +9,19 @@ from libensemble.tools import save_libE_output, add_unique_random_streams
 from libensemble.executors.mpi_executor import MPIExecutor
 
 from libensemble.alloc_funcs.start_only_persistent import only_persistent_gens
-from libe_opt.persistent_gp import persistent_gp_mf_disc_gen_f  # modified for larger resource set variation
+from libe_opt.persistent_gp import persistent_gp_mf_disc_gen_f
 from libe_opt.sim_functions import run_simulation
 from libensemble import logger
 
 logger.set_level('DEBUG')
 
-# SH TODO: Also need to make changes to the template_simulation_script including MPI and num steps.
-
 gen_type = 'bo'
-sim_max = 20  ##### {{ sim_max }}
-run_async = True  ####### {{ run_async }}
-max_rsets_per_worker = 4  #{{max_rsets_per_worker}}
-custom_info={'mpi_runner':'srun'}  # {{ mpi_runner }}  #tempalte this - but ideally  only want to set custom_info if you want it...
-USE_CUDA_VISIBLE_DEVICES = False  # {{set_CUDA_VISIBLE_DEVICES}}
-MPICH_GPU_SUPPORT = True  # {{MPICH_GPU_SUPPORT}}
+sim_max = {{ sim_max }}
+run_async = {{ run_async }}
+max_rsets_per_worker = {{max_rsets_per_worker}}
+
+USE_CUDA_VISIBLE_DEVICES = {{ cuda_visible_devices }}
+MPICH_GPU_SUPPORT = {{ mpich_gpu_support }}
 
 nworkers, is_master, libE_specs, _ = parse_args()
 sim_template = 'template_simulation_script.py'
@@ -57,7 +54,8 @@ gen_specs={'gen_f': persistent_gp_mf_disc_gen_f,
            'user': {'gen_batch_size': 8,
                     'lb': np.array([0.7, 3. , 0.6, 0.1]),
                     'ub': np.array([1.05, 7.5 , 0.8 , 1.5 ]),
-                    'name': 'resolution', 'range': [2.0, 4.0],
+                    'name': 'resolution',
+                    'range': [2.0, 4.0],
                     'discrete': True,
                     'cost_func': lambda z: z[0][0]**3,
                     'max_rsets_per_worker': max_rsets_per_worker
@@ -66,12 +64,12 @@ gen_specs={'gen_f': persistent_gp_mf_disc_gen_f,
 
 alloc_specs={'alloc_f': only_persistent_gens,
              'out': [('given_back', bool)],
-             'user': {'async_return': True}}
+             'user': {'async_return': run_async}}
 
 
 libE_specs['sim_dirs_make'] = True
 libE_specs['sim_dir_copy_files'] = [sim_template]
-libE_specs['save_every_k_sims'] = 5
+# libE_specs['save_every_k_sims'] = 5
 libE_specs['dedicated_mode'] = False   # False is default.
 libE_specs['zero_resource_workers'] = [1]
 
@@ -79,7 +77,11 @@ exit_criteria = {'sim_max': sim_max}  # Exit after running sim_max simulations
 
 
 # Setup MPI executor
-exctr = MPIExecutor(custom_info=custom_info)
+{% if mpi_runner is defined %}
+    exctr = MPIExecutor(custom_info={'mpi_runner': {{ mpi_runner }} })
+{% else %}
+    exctr = MPIExecutor()
+
 exctr.register_app(full_path='simulation_script.py', calc_type='sim')
 
 # Create a different random number stream for each worker and the manager
