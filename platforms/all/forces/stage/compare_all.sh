4d3832382e98e7847993dd011b9cf030ae6c0a6b
jlnav <jnavarro@anl.gov>
2022-04-27 15:57:11 -0500

    update compare_all.sh script?

--- platforms/all/forces/stage/compare_all.sh
+++ platforms/all/forces/stage/compare_all.sh
@@ -2,6 +2,6 @@ for dir in test_*
 do
   cd $dir
   echo -e "Comparing $dir:"
-   ../compare_npy.py run_libe_forces_results_History_*.npy
+   ../compare_npy.py run_libe_forces_history_*.npy
   cd ..
 done
