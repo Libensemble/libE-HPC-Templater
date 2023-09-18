b3ad811a5d943306bcd672f49a1b442f1d857a8e
jlnav <jnavarro@anl.gov>
2023-09-18 12:33:07 -0500

    finish setting up test params and submission script

--- platforms/perlmutter/gpu_forces/stage/build_forces.sh
+++ platforms/perlmutter/gpu_forces/stage/build_forces.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mpicc -DGPU -O3 -fopenmp -mp=gpu -o forces.x forces.c
+cc -DGPU -Wl,-znoexecstack -O3 -fopenmp -mp=gpu -target-accel=nvidia80 -o forces_gpu.x forces.c
+cc -Wl,-znoexecstack -O3 -fopenmp -o forces_cpu.x forces.c
