38c427649fca6531c62168e4a9ddb8a6d07e6e20
jlnav <jnavarro@anl.gov>
2023-09-18 10:29:50 -0500

    remove Theta, as its being decommissioned 12/23

--- platforms/theta/forces/stage/build_forces.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cc -O3 -o forces.x forces.c
-
--- platforms/theta/forces/stage/prepare_files_for_balsam2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for dir in test_MPI_balsam2_*; do
-        cp forces.c $dir
-        cp define_apps.py $dir
-        cp forces_simf_balsam.py $dir
-        cp build_forces.sh $dir
-done
